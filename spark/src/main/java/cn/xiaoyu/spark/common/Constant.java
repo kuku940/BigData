6dfec10c7f96394ee7259ed210c639bcbd6ec36b
01399268 <xiaoyu3@sfmail.sf-express.com>
2021-11-02 11:26:29 +0800

    Initial commit

---
+++ spark/src/main/java/cn/xiaoyu/spark/common/Constant.java
@@ -0,0 +1,7 @@
+package cn.xiaoyu.spark.common;
+
+import java.io.File;
+
+public class Constant {
+    public static final String LOCAL_FILE_PREX = "file:///" + new File(".").getAbsolutePath();
+}
---
+++ spark/src/main/java/cn/xiaoyu/spark/utils/PrintUtils.java
@@ -0,0 +1,11 @@
+package cn.xiaoyu.spark.utils;
+
+public class PrintUtils {
+    public static void print(Object msg) {
+        System.out.println(msg);
+    }
+
+    public static void print() {
+        System.out.println();
+    }
+}
